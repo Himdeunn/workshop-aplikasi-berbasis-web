86e6052a4d07ae3a3c57175fdcc9e4a5ff701b62
ThePeaces <tegarmascart@gmail.com>
2025-09-11 18:10:01 +0700

    Adding Employee Migration and Models

---
+++ laravel/database/migrations/2025_09_05_132000_create_employees_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('fullname', 100);
+            $table->string('email', 100);
+            $table->string('phone_number', 15);
+            $table->date('birth_date');
+            $table->text('address');
+            $table->text('date_entry');
+            $table->enum('status', ['active', 'non-active'])->default('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
